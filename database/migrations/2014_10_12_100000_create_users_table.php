e7520e1dfd7f5526228dbb1cb0c66b7a05791b6d
KuchiiMaw <lotsaspaghetti95@gmail.com>
2019-02-01 14:08:38 +0100

    Migració BD

---
+++ database/migrations/2014_10_12_100000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('role');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email', 100)->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'role' => 'admin',
+                'name' => 'admin',
+                'surname' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => '@dm1n'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
