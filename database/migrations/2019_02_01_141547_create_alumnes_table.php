a062f299b4e292a5699bfaef38500c716e6396d4
Jaume Balaña <jaumewowcore@gmail.com>
2019-02-01 14:21:00 +0100

    Table alumnes

---
+++ database/migrations/2019_02_01_141547_create_alumnes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('dni');
+            $table->integer('course');
+            $table->date('dob');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnes');
+    }
+}
