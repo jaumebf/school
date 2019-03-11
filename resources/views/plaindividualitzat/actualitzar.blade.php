ad77aaff300f0ecf2f0de590e1ddbe898d329691
Jaume Balaña <jaumewowcore@gmail.com>
2019-03-11 14:26:13 +0100

    Pla individualitzar apunt per fer el actualitzar

--- resources/views/plaindividualitzat/actualitzar.blade.php
+++ resources/views/plaindividualitzat/actualitzar.blade.php
@@ -16,50 +16,81 @@
     <div class="col-md-10">
     <form method="POST" action="{{url('alumnes/actualitzar')}}">
     @csrf            
-            <input type="hidden" name="id" value="{{$alumne->id}}">
-    
-            <div class="form-group" name="llengua">
-                <label for="llengua" class="col-sm-2 col-form-label col-form-label-sm">Llengua</label>               
-                <input type="checkbox" name="llengua" 
-                       value="@if($pla && $pla->llengua == 1) checked; @endif" class="form-control"> 
-                    {{"La llengua es".$pla->llengua}}              
+            <input type="hidden" name="id" value="{{$pla->id}}">
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->llengua == 1) checked @endif>
+                <label class="form-check-label" for="llengua">
+                  Llengua
+                </label>                                        
+            </div>
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->llengua_castellana == 1) checked @endif>
+                <label class="form-check-label" for="llengua_castellana">
+                  Llengua castellana
+                </label>                                        
             </div>
             
-
-            <div class="form-group" name="cognom">
-                <label for="cognom" class="col-sm-2 col-form-label col-form-label-sm">Ll castellana</label>               
-                <input type="text" name="cognom" value="{{$alumne->surname}}" class="form-control">
-                @if($errors->has('cognom'))                  
-                <strong>{{ $errors->first('cognom') }}</strong>                  
-                @endif    
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->llengua_inglesa == 1) checked @endif>
+                <label class="form-check-label" for="llengua_inglesa">
+                  Llengua inglesa
+                </label>                                        
             </div>
-
-            <div class="form-group" name="dni">
-                <label for="dni" class="col-sm-2 col-form-label col-form-label-sm">DNI</label>               
-                <input type="text" name="dni" value="{{$alumne->dni}}" class="form-control">
-                @if($errors->has('dni'))                  
-                <strong>{{ $errors->first('dni') }}</strong>                  
-                @endif    
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->matematiques == 1) checked @endif>
+                <label class="form-check-label" for="matematiques">
+                  Matematiques
+                </label>                                        
             </div>
-
-            <div class="form-group" name="curs">
-                <label for="curs" class="col-sm-2 col-form-label col-form-label-sm">Curs</label>               
-                <input type="number" name="curs" value="{{$alumne->course}}" class="form-control">
-                @if($errors->has('cognom'))                  
-                <strong>{{ $errors->first('curs') }}</strong>                  
-                @endif   
+                            
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->cm_natural == 1) checked @endif>
+                <label class="form-check-label" for="cm_natural">
+                  CM Natural
+                </label>                                        
             </div>
-
-            <div class="form-group" name="dob">
-                <label for="dob" class="col-sm-2 col-form-label col-form-label-sm">Data de naixement</label>               
-                <input type="date" name="dob" value="{{$alumne->dob}}" class="form-control">
-                @if($errors->has('dob'))                  
-                <strong>{{ $errors->first('dob') }}</strong>                  
-                @endif                 
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->cm_social == 1) checked @endif>
+                <label class="form-check-label" for="cm_social">
+                  CM social
+                </label>                                        
             </div>
             
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->ed_artistica == 1) checked @endif>
+                <label class="form-check-label" for="ed_artistica">
+                  Ed. Artistica
+                </label>                                        
+            </div>
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->ed_fisica == 1) checked @endif>
+                <label class="form-check-label" for="ed_fisica">
+                  Ed. Física
+                </label>                                        
+            </div>
+            
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->religio == 1) checked @endif>
+                <label class="form-check-label" for="religio">
+                  Religio
+                </label>                                        
+            </div>
+               
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" @if($pla && $pla->valors == 1) checked @endif>
+                <label class="form-check-label" for="valors">
+                  Valors
+                </label>                                        
+            </div>
+               
+                      
             <br>
-            <button type="submit" class="btn btn-primary">Guardar alumne</button>
+            <button type="submit" class="btn btn-primary">Guardar Pla Individualitzat</button>
             </fieldset>
         </form>
     </div>
