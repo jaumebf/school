a4a48d49858e43190f30aefb44229ac3450e9786
KuchiiMaw <lotsaspaghetti95@gmail.com>
2019-03-29 14:36:12 +0100

    Comensar llista assignatura

--- database/migrations/2019_02_08_133705_create_assignatura_table.php
+++ database/migrations/2019_02_08_133705_create_assignatura_table.php
@@ -15,24 +15,38 @@ class CreateAssignaturaTable extends Migration
     {
         Schema::create('assignatura', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('actitud_1');
-            $table->integer('actitud_2');
-            $table->integer('actitud_3');
-            $table->integer('esforc_1');
-            $table->integer('esforc_2');
-            $table->integer('esforc_3');
-            $table->integer('treball_1');
-            $table->integer('treball_2');
-            $table->integer('treball_3');   
-            $table->integer('deures_1');
-            $table->integer('deures_2');
-            $table->integer('deures_3');   
-            $table->integer('qualificacio_1');
-            $table->integer('qualificacio_2');
-            $table->integer('qualificacio_3');
-            $table->text('observacions');
+            $table->string('nom');
+            $table->integer('actitud_1')->default(0);
+            $table->integer('actitud_2')->default(0);
+            $table->integer('actitud_3')->default(0);
+            $table->integer('esforc_1')->default(0);
+            $table->integer('esforc_2')->default(0);
+            $table->integer('esforc_3')->default(0);
+            $table->integer('treball_1')->default(0);
+            $table->integer('treball_2')->default(0);
+            $table->integer('treball_3')->default(0);   
+            $table->integer('deures_1')->default(0);
+            $table->integer('deures_2')->default(0);
+            $table->integer('deures_3')->default(0);   
+            $table->integer('qualificacio_1')->default(0);
+            $table->integer('qualificacio_2')->default(0);
+            $table->integer('qualificacio_3')->default(0);
+            $table->text('observacions')->default(' ');
             $table->timestamps();
         });
+        
+        DB::table('assignatura')->insert(
+            array('nom' => "Llengua catalana"),
+            array('nom' => "Llengua castellana"),
+            array('nom' => "Llengua anglesa"),
+            array('nom' => "Matemàtiques"),
+            array('nom' => "Coneixement del medi natural"),
+            array('nom' => "Coneixement del medi social"),
+            array('nom' => "Visual i plàstica, música i dansa"),
+            array('nom' => "Educació física"),
+            array('nom' => "Religió catòlica"),
+            array('nom' => "Educació valors socials i cívics")                
+        );
     }
 
     /**
